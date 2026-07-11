e5147c2c8b535cccac30d211dfb86ae46afb83d9
Uday Prasad <udayprasad174396@gmail.com>
2026-07-11 23:32:22 +0530

    Added space for map

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,12 @@
+import Map from '../assets/map.jpg';
+
 export function Body(){
     return (
         <>
             <div>
+                <div className='w-full [h-70vh]'>
+                    <img src={Map} alt="Map Icon" />
+                </div>
                 <p> Your Current Bus Stop is : Bus stop name  dd for change bus stop<br />
                     Estimated time of arrival of current bus/Crowd level :  <button>book tickets?</button><br />
                     Estimated time of arrival of next bus/ Crowd level :   <button>book tickets?</button><br />
